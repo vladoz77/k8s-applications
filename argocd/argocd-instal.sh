7a06a30c87766b12246d3d9dfc2713dfb11ba92a
Kovalev Vladislav <vladoz77@yandex.ru>
2023-12-07 14:43:02 +0300

    add check if argo installed

--- argocd/argocd-instal.sh
+++ argocd/argocd-instal.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
 #Install argocd manifest
+kubectl get ns argocd > /dev/null 2>&1
+if [ "${?}" -eq 0 ]
+then
+  echo 'argocd is installed'
+  exit 1
+fi
+
 kubectl create namespace argocd > /dev/null
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml > /dev/null
 sleep 10
@@ -24,11 +31,11 @@ EOF
 kubectl create ingress -n argocd argocd-server-ingress --rule "${ARGOCD_HOST}/*=argocd-server:80"
 echo ingress have created
 
-# Get init admin password
+# Get init admin password and update
 sleep 30
 ADMIN_INIT_PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
 
 # Change admin password
 read -p 'Enter new admin password ' NEW_PASS
-argocd login argocd.dev.local --username admin --password ${ADMIN_INIT_PASS} --insecure --grpc-web
+argocd login argocd.dev.local --username admin --password ${ADMIN_INIT_PASS} --insecure 
 argocd account update-password --account admin --current-password  ${ADMIN_INIT_PASS}  --new-password ${NEW_PASS}
